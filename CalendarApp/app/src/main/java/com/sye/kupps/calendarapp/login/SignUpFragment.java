1d9f0ad2f6b48f706002e6cbbab47d12eb535656
rgibsond <r.gibson.drysdale@gmail.com>
2016-11-07 19:11:10 -0500

    app activity test code commit

--- CalendarApp/app/src/main/java/com/sye/kupps/calendarapp/login/SignUpFragment.java
+++ CalendarApp/app/src/main/java/com/sye/kupps/calendarapp/login/SignUpFragment.java
@@ -24,7 +24,7 @@ public class SignUpFragment extends Fragment {
 
     public static final String LOG_TAG = SignUpFragment.class.getName();
 
-    Button registerButton, backToSignInButton;
+    Button registerButton, backToSignInButton, bypass;
     EditText usernameInput, passwordInput, passwordRepeat;
 
     @Override
@@ -38,6 +38,7 @@ public class SignUpFragment extends Fragment {
         usernameInput = (EditText) root.findViewById(R.id.register_username_text_field);
         passwordInput = (EditText) root.findViewById(R.id.register_password_text_field);
         passwordRepeat = (EditText) root.findViewById(R.id.register_repeat_password_text_field);
+        bypass = (Button) root.findViewById(R.id.bypass);
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +63,14 @@ public class SignUpFragment extends Fragment {
                 // re-replace this fragment with a new Login Fragment
                 FragmentHandler fh = new FragmentHandler(getActivity());
                 fh.replace(R.id.login_activity_container, new LoginFragment());
+            }
+        });
+
+        bypass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), PlaceholderActivity.class);
+                startActivity(intent);
             }
         });
 
