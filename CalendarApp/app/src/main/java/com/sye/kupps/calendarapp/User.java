ba02802028d3d978e1427fdcf67b6e027f5c0d66
Jamie Kuppel <jamiekuppel@gmail.com>
2016-11-08 23:24:29 -0500

    Organized User class

--- CalendarApp/app/src/main/java/com/sye/kupps/calendarapp/User.java
+++ CalendarApp/app/src/main/java/com/sye/kupps/calendarapp/User.java
@@ -9,24 +9,54 @@ import java.util.ArrayList;
  */
 public class User {
 
+    /** The username of this user. */
     private String username;
 
+    /** A list of the events this user is attending/hosting. */
+    private ArrayList<Event> events;
+
+    /** A list of the friends of this user. */
     private ArrayList<Integer> friends;
 
-    public User(String userString) {
-
+    /**
+     * Used to build a User from a string unique to a specific user.
+     *
+     * @param user The string to build the user from.
+     * @param newUser Determines whether or not to treat user as a username or user recovery string
+     */
+    public User(String user, boolean newUser) {
+        if (newUser)
+            generateNewUser(user);
+        else
+            restoreUser(user);
     }
 
-    public User() {
+    /**
+     * Instantiates fields from the database information
+     *
+     * @param username The username from which to pull data
+     */
+    private void generateNewUser(String username) {
+        // TODO
+    }
 
+    /**
+     * Instantiates fields from parsing the userString
+     *
+     * @param userString The userString to parse
+     */
+    private void restoreUser(String userString) {
+        // TODO
     }
 
     /**
      * Returns a string for the purpose of storing and recovering user information.
+     *
      * @return The recovery string
      */
     @Override
     public String toString() {
+        // TODO
         return null;
     }
 
